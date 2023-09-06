06cf89bb9b38e8ce586bf698b7f7c451d7bfb30b
KarlBager <kallebager@gmail.com>
2023-09-06 14:30:49 +0200

    knapper virker, tøj-img'er er positioneret rigtigt

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ function startGame(){
     startButton = document.getElementById('start-button');
     startButton.style.display = "none";
 }
+
+
+function chooseClothing(choice, type){
+    console.log(choice);
+    console.log(type);
+
+    //Highlight valgte knap og unhighlight den gamle
+    for(var i=1; i<4; i++){
+        if(choice==i){
+            let chosenButtonId = type + "-button" + choice;
+            console.log(chosenButtonId);
+            chosenButton = document.getElementById(chosenButtonId);
+            console.log(chosenButton);
+            chosenButton.style.backgroundColor = "#BBBBBB";
+        } else{
+            let unchosenButtonId = type + "-button" + i;
+            console.log(unchosenButtonId);
+            unchosenButton = document.getElementById(unchosenButtonId);
+            console.log(unchosenButton);
+            unchosenButton.style.backgroundColor = "#EEEEEE";
+        }
+    }
+}
